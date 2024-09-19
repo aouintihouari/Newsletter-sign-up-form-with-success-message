0f3c8c9645acf1a2fae8325a3b8dfaeebbe89a29
aouintihouari <169095690+aouintihouari@users.noreply.github.com>
2024-09-19 18:19:42 +0100

    Initial Commit
    Add responsive Newsletter Sign-up Form with success message using HTML, CSS, and JavaScript.

---
+++ script.js
@@ -0,0 +1,44 @@
+const form = document.getElementById("form");
+const emailInput = form.querySelector("#email");
+const errorMessage = form.querySelector(".error-message");
+const successComponent = document.querySelector(".success-component");
+const successMessageBtn = document.querySelector(".dismiss");
+
+function isValidEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+function toggleId(element, idName) {
+  if (element.id === idName) {
+    element.removeAttribute("id");
+  } else {
+    element.id = idName;
+  }
+}
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const emailValue = emailInput.value.trim();
+
+  if (!emailValue || !isValidEmail(emailValue)) {
+    errorMessage.textContent = "Valid Email Required";
+
+    emailInput.style.cssText = `
+      color: rgb(255, 98, 87);
+      background-color: rgba(255, 98, 87, .3);
+      border: 1px solid rgb(255, 98, 87);
+    `;
+  } else {
+    errorMessage.textContent = "";
+    emailInput.style.cssText = "";
+    toggleId(form, "hide");
+    toggleId(successComponent, "hide");
+  }
+});
+
+successMessageBtn.addEventListener("click", () => {
+  toggleId(form, "hide");
+  toggleId(successComponent, "hide");
+});
